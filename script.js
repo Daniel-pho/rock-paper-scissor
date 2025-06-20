12c512557298db19d26f75b2628d1296d903886f
Daniel P. <daniel.phou@hotmail.com>
2025-06-20 11:24:43 -0400

    implement logic for buttons and finishing game

--- script.js
+++ script.js
@@ -27,24 +27,32 @@ function getHumanChoice() {
     return choice
 }
 
+
 function playRound(humanChoice, computerChoice) {
+    const resultsDiv = document.querySelector(".roundResults")
+
     if (humanChoice === computerChoice) {
-        console.log("It's a Draw!");
+        resultsDiv.textContent = "It's a Draw!"
+        // console.log("It's a Draw!");
     }
     else if (humanChoice === "scissor" && computerChoice === "paper") {
-        console.log("Player wins this round")
+        resultsDiv.textContent = "Player wins this round"
+        // console.log("Player wins this round")
         return humanScore++;
     }
     else if (humanChoice === "paper" && computerChoice === "rock") {
-        console.log("Player wins this round")
+        resultsDiv.textContent = "Player wins this round"
+        // console.log("Player wins this round")
         return humanScore++;
     }
     else if (humanChoice === "rock" && computerChoice === "scissor") {
-        console.log("Player wins this round")
+        resultsDiv.textContent = "Player wins this round"
+        // console.log("Player wins this round")
         return humanScore++;
     }
     else {
-        console.log("Computer wins this round");
+        resultsDiv.textContent = "Computer wins this round"
+        // console.log("Computer wins this round");
         computerScore++
     }
 }
@@ -104,23 +112,73 @@ paperBtn.textContent = "Paper"
 scissorBtn.textContent = "Scissor"
 document.body.append(rockBtn, paperBtn, scissorBtn);
 
+const humanDiv = document.querySelector(".playerScore");
+const computerDiv = document.querySelector(".computerScore");
+humanDiv.append(humanScore)
+// console.log(humanDiv, "human score");
+
+function gameFinish(player) {
+    const resultsDiv = document.querySelector(".roundResults")
+    const finishDiv = document.createElement('div')
+    finishDiv.textContent = `${player} wins!`;
+    document.body.append(finishDiv);
+    rockBtn.remove()
+    paperBtn.remove()
+    scissorBtn.remove()
+    resultsDiv.remove()
+
+
+    // function playAgain() {
+    //     humanScore = 0;
+    //     computerScore = 0;
+    //     document.body.append(rockBtn, paperBtn, scissorBtn);
+    //     playButton.remove()
+    // }
+    // const playButton = document.createElement("button")
+    // playButton.textContent = "Play Again";
+    // document.body.append(playButton);
+    // playButton.addEventListener("click", () => {
+    //     playAgain()
+    // })
+}
+
+function updateScores() {
+    humanDiv.textContent = `Player Score: ${humanScore}`;
+    computerDiv.textContent = `Computer Score: ${computerScore}`;
+
+    if (humanScore === 5) {
+        console.log("player wins");
+        gameFinish("player")
+    }
+    if (computerScore === 5) {
+        gameFinish("Computer")
+    }
+}
+
+
 
 rockBtn.addEventListener("click", () => {
     let choice = "rock"
     let computerChoice = getComputerChoice();
     playRound(choice, computerChoice);
+    updateScores();
+
 })
 
 paperBtn.addEventListener("click", () => {
     let choice = "paper";
     let computerChoice = getComputerChoice();
     playRound(choice, computerChoice);
+    updateScores();
 });
 
 scissorBtn.addEventListener("click", () => {
     let choice = "scissor";
     let computerChoice = getComputerChoice();
     playRound(choice, computerChoice);
+    updateScores();
 });
+
+
 
 // console.log(getHumanChoice());
